0a7cead5d1d291b31e5ac9a730509081f18f97d2
Damon Leven <mcwertgaming@gmail.com>
2020-07-05 13:51:17 +0200

    fixed missing namespace identifier

--- src/folf-namespace/benchmarking.cpp
+++ src/folf-namespace/benchmarking.cpp
@@ -5,11 +5,11 @@ std::chrono::time_point<std::chrono::high_resolution_clock> folf::timeOperations
 {
     return std::chrono::high_resolution_clock::now();
 }
-timeBench::timeBench()
+folf::timeBench::timeBench()
 {
     startTime = folf::timeOperations::getTimestamp();
 }
-timeBench::~timeBench()
+folf::timeBench::~timeBench()
 {
     long double takenTime = std::chrono::time_point_cast<std::chrono::microseconds>(folf::timeOperations::getTimestamp()).time_since_epoch().count() - std::chrono::time_point_cast<std::chrono::microseconds>(startTime).time_since_epoch().count();
     long double seconds = takenTime / 1000000;
