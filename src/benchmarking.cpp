9575bbc0acceb8456e3a4bd7149d30ba8968d112
Damon Leven <mcwertgaming@gmail.com>
2020-07-11 21:32:05 +0200

    added message for starting and stoping benchmarking

--- src/benchmarking.cpp
+++ src/benchmarking.cpp
@@ -9,11 +9,13 @@ std::chrono::time_point<std::chrono::high_resolution_clock> folf::timeOperations
 }
 folf::timeBench::timeBench()
 {
+    std::cout << "Benchmark started!\n";
     startTime = folf::timeOperations::getTimestamp();
 }
 folf::timeBench::~timeBench()
 {
     long double takenTime = std::chrono::time_point_cast<std::chrono::microseconds>(folf::timeOperations::getTimestamp()).time_since_epoch().count() - std::chrono::time_point_cast<std::chrono::microseconds>(startTime).time_since_epoch().count();
     long double seconds = takenTime / 1000000; // calculates the milliseconds of the timestamp into seconds
+    std::cout << "Benchmark finished!\n";
     std::cout << "Time: " << seconds << " seconds\n";
 }
