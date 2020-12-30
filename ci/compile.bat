46fc9496abe227f796b99992d99f87dcd8972ad0
damon <mcwertgamingQgmail.com>
2020-12-30 17:48:28 +0000

    added binary testing on windows as well

--- ci/compile.bat
+++ ci/compile.bat
@@ -1,3 +1,7 @@
 cmake -DCMAKE_INSTALL_PREFIX=%cd%\inst .
 cmake --build . --config Release
 cmake --build . --config Debug
+
+.\examples\console-utils-test.exe
+.\examples\prime-sum-benchmark.exe
+.\examples\time-operations.exe
