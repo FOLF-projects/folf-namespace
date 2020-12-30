9660a4c677b8f99696a9f54c917a4c502a72a28c
damon <mcwertgamingQgmail.com>
2020-12-30 17:01:16 +0000

    simplyfied paths

--- ci/compile.sh
+++ ci/compile.sh
@@ -4,3 +4,8 @@ set -ex
 cmake -DCMAKE_INSTALL_PREFIX=`pwd`/inst .
 cmake --build . --config Release
 cmake --build . --config Debug
+
+# run the programs
+./examples/console-utils-test
+./examples/prime-sum-benchmark
+./examples/time-operations
