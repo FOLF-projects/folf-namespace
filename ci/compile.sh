c442f672dc57a938c1bdff0e58bc424c6817aa8c
damon <mcwertgamingQgmail.com>
2020-12-30 17:22:55 +0000

    switched to clang on linux/macos build to fix OpenMP error on macos

--- ci/compile.sh
+++ ci/compile.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -ex
 
+// use clang for compilation to fix OpenMP error on macos
+export CC=/usr/bin/clang
+export CXX=/usr/bin/clang++
+
 cmake -DCMAKE_INSTALL_PREFIX=`pwd`/inst .
 cmake --build . --config Release
 cmake --build . --config Debug
