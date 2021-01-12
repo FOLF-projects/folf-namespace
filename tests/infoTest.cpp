3859b0ff474aa3175adf8a13416856864bbc34d4
damon <mcwertgamingQgmail.com>
2021-01-12 21:31:32 +0000

    removed info test

--- tests/infoTest.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-
-// testing for folf's info header
-
-// example test, TEST(TEST_SUITE, TEST_NAME) << "OPTIONAL TEST DESCRIPTION"
-// TEST(timeToolsTest, exampleTest)
-// {
-//     EXPECT_EQ(true,true) << "tests, if true is really true";
-// }
