476e4378f20c7c732e0fabe9c7f3945b715bf551
AdmiralEmser <61253950+AdmiralEmser@users.noreply.github.com>
2021-01-31 22:48:44 +0100

    Update algorithmsTest.cpp

--- tests/algorithmsTest.cpp
+++ tests/algorithmsTest.cpp
@@ -3,7 +3,7 @@
 // testing for folf's algorithms header
 
 // example test, TEST(TEST_SUITE, TEST_NAME) << "OPTIONAL TEST DESCRIPTION"
-// TEST(timeToolsTest, exampleTest)
-// {
-//     EXPECT_EQ(true,true) << "tests, if true is really true";
-// }
+TEST(timeToolsTest, exampleTest)
+{
+    EXPECT_EQ(euclidean_algorithm(442, 255),17) << "Test euclidean algorithm";
+}
