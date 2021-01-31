7fe657a6df126923227a4b6bfa91d7a92d6f02eb
AdmiralEmser <61253950+AdmiralEmser@users.noreply.github.com>
2021-01-31 23:19:26 +0100

    Update calcToolsTest.cpp

--- tests/calcToolsTest.cpp
+++ tests/calcToolsTest.cpp
@@ -3,7 +3,20 @@
 // testing for folf's calcTools header
 
 // example test, TEST(TEST_SUITE, TEST_NAME) << "OPTIONAL TEST DESCRIPTION"
-// TEST(timeToolsTest, exampleTest)
-// {
-//     EXPECT_EQ(true,true) << "tests, if true is really true";
-// }
+TEST(timeToolsTest, exampleTest)
+{
+    const unsigned long long primeSeven = 7;
+    EXPECT_EQ(checkPrime(&primeSeven),true) << "Checks that 7 is a prime";
+    
+    const unsigned long long primeTwelve = 12;
+    EXPECT_EQ(checkPrime(&primeTwelve),true) << "Checks that 12 is no prime";
+  
+    const unsigned long long maxNum = 1000000;
+    const unsigned long long primeSum;
+    primeSum(&maxNum, &primeSum)
+    EXPECT_EQ(primeSum,3203324994357) << "Tests primeSum";
+    
+    const unsigned int numOne = 3;
+    const unsigned int numTwo = 2;
+    EXPECT_EQ(createVector(&numOne, &numTwo),(numOne - numTwo)) << "Tests vector function";
+}
